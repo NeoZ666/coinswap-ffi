cb146fd923e2ce7bbda8fa130012b24cca12697c
NeoZ666 <neoz.blockchain@gmail.com>
2026-07-23 20:01:34 +0530

    new update
    Signed-off-by: NeoZ666 <neoz.blockchain@gmail.com>

--- coinswap-js/src/taker.rs
+++ coinswap-js/src/taker.rs
@@ -392,7 +392,7 @@ impl Taker {
       .map_err(|e| napi::Error::from_reason(format!("Failed to acquire taker lock: {}", e)))?;
     let wallet = taker
       .get_wallet()
-      .read()
+      .write()
       .map_err(|e| napi::Error::from_reason(format!("Failed to acquire wallet lock: {}", e)))?;
     let internal_addresses = wallet
       .get_next_internal_addresses(count, cs_address_type)
